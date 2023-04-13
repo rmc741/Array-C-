8b89840434ee56d768ce32149441c45db3d5b08e
RafaelMendonca <rafael.medonca2@gmail.com>
2023-04-13 15:07:54 -0300

    Primeiro commit estudando Arrays em C#

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 20;
+    idades[2] = 40;
+    idades[3] = 11;
+    idades[4] = 25;
+
+    Console.WriteLine($"Tamanho do Array é igual a: {idades.Length}");
+
+    for ( int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"indice [{i}] = {idade}");
+    }
+}
+
