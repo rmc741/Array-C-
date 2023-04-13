9b7cce1c1195afaaaaa89b3523640f5c89eb28fd
RafaelMendonca <rafael.medonca2@gmail.com>
2023-04-13 15:29:36 -0300

    Exercicio de array com int e strings

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-
-TestaArrayInt();
+//TestaArrayInt();
+//TesteBuscarPalavras();
 
 void TestaArrayInt()
 {
@@ -13,10 +13,38 @@ void TestaArrayInt()
 
     Console.WriteLine($"Tamanho do Array é igual a: {idades.Length}");
 
+    int acumulador = 0;
     for ( int i = 0; i < idades.Length; i++)
     {
         int idade = idades[i];
         Console.WriteLine($"indice [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador/ idades.Length;
+    Console.WriteLine($"media é igual: {media}");
+}
+
+void TesteBuscarPalavras()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for ( int i = 0;i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1} Palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.WriteLine("Digite a palavra a ser encontrada:");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
     }
 }
 
